459854ea66a069413b09d9c5853f4751495f0097
hermon 22 <hermontesfa19@gmail.com>
2021-09-13 10:46:56 +0200

    some changes made

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,8 +5,7 @@ require('dotenv').config();
 
 const url = process.env.MONGO_URI
 
-// database connection
-const dbURI = url;   
-mongoose.connect(`${dbURI}`, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
+
+mongoose.connect( url , { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex:true })
     .then(console.log('connected to db'))
     .catch((err) => console.log(err));
